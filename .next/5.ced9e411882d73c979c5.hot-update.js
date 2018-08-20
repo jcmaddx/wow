348a64ff599e964d8e5780bbc1a5fe89bcedc975
JC Maddox <jcmaddx@gmail.com>
2018-08-20 10:36:52 -0700

    ignored a few more things

--- .next/5.ced9e411882d73c979c5.hot-update.js
+++
@@ -1,2 +0,0 @@
-webpackHotUpdate(5,[])
-//# sourceMappingURL=5.ced9e411882d73c979c5.hot-update.js.map
--- .next/5.f9c5fd9e4f1260fb36dd.hot-update.js
+++
@@ -1,2 +0,0 @@
-webpackHotUpdate(5,[])
-//# sourceMappingURL=5.f9c5fd9e4f1260fb36dd.hot-update.js.map
