05050d079d7037af7997c8f547e8083923eb32de
jmaddox <jmaddox@ncsoft.com>
2018-08-26 16:06:32 -0700

    Action bar

--- components/Hud/ActionBar.js
+++ components/Hud/ActionBar.js
@@ -1,6 +1,33 @@
-
+import './bar.scss'
 const ActionBar = (props) => (
-	<div id="actionbar">Action Bar</div>
+	<div id="actionbar">
+		<div className="bar-frame">
+			<div className="skills">
+				<div className="primary">
+					<div className="skill"><img src="/static/images/icons/bars/immolate.png" /></div>
+					<div className="skill"><img src="/static/images/icons/bars/conflagrate.png" /></div>
+					<div className="skill"><img src="/static/images/icons/bars/incinerate.png" /></div>
+					<div className="skill"><img src="/static/images/icons/bars/chaos.png" /></div>
+					<div className="skill"><img src="/static/images/icons/bars/havoc.png" /></div>
+					<div className="skill"><img src="/static/images/icons/bars/rain.png" /></div>
+					<div className="skill"><img src="/static/images/icons/bars/void.png" /></div>
+					<div className="skill"><img src="/static/images/icons/bars/infernal.png" /></div>
+					<div className="skill"><img src="/static/images/icons/bars/soulstone.png" /></div>
+					<div className="skill"><img src="/static/images/icons/bars/healthstone.png" /></div>
+					<div className="skill"><img src="/static/images/icons/bars/funnel.png" /></div>
+					<div className="skill"><img src="/static/images/icons/bars/siphon.png" /></div>
+				</div>
+				<div className="secondary">
+					<div className="skill"><img src="/static/images/icons/bars/burn.png" /></div>
+					<div className="skill"><img src="/static/images/icons/bars/ritual.png" /></div>
+					<div className="skill"><img src="/static/images/icons/bars/portal.png" /></div>
+					<div className="skill"><img src="/static/images/icons/bars/dalaran.png" /></div>
+					<div className="skill"><img src="/static/images/icons/bars/hearth.png" /></div>
+				</div>
+			</div>
+		</div>
+		<div className="ap-bar"></div>
+	</div>
 )
 
 export default ActionBar
--- components/Hud/Player.js
+++ components/Hud/Player.js
@@ -5,7 +5,7 @@ const Player = (props) => {
 			<div className="image"></div>
 			<div className="name">{props.name}</div>
 			<div className="bar health"><span>100%</span>{parseInt(props.health).toLocaleString()}</div>
-			<div className="bar mana"><span>100%</span>{parseInt(props.mana).toLocaleString()}</div>
+			<div className="bar mana"><span>100%</span>{(parseInt(props.mana) / 1000) + " K"}</div>
 			<div className="level">{props.level}</div>
 		</div>
 	)
