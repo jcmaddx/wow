ee3bfc2807508a5e3dc3009f7d754320e75f5de7
jmaddox <jmaddox@ncsoft.com>
2018-09-01 11:16:25 -0700

    Fixed player portrait z-index

--- components/Hud/Player.js
+++ components/Hud/Player.js
@@ -2,7 +2,8 @@ import "./player.scss"
 const Player = (props) => {
 	return(
 		<div id="player">
-			<div className="image"></div>
+			<div className="frame" />
+			<div className="image" />
 			<div className="name">{props.name}</div>
 			<div className="bar health"><span>100%</span>{parseInt(props.health).toLocaleString()}</div>
 			<div className="bar mana"><span>100%</span>{(parseInt(props.mana) / 1000) + " K"}</div>
